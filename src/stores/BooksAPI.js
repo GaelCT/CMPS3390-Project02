4dddba510fe8c763fccbda96e5d31658ec9356cd
Armando Vargas <avargas88@csub.edu>
2025-10-08 12:29:41 -0700

    fixed the search

--- src/stores/BooksAPI.js
+++ src/stores/BooksAPI.js
@@ -9,7 +9,8 @@ export const useBookStore = defineStore('bookResults', {
         async getBooks(search){ // uses input of a search
             try{
                 const query = encodeURIComponent(search.trim()) // saves the search to a value
-                const response = await axios.get('https://openlibrary.org/search.json?q=${query}') 
+                console.log(query)
+                const response = await axios.get("https://openlibrary.org/search.json?title=" + query) 
                 this.books = response.data.docs
             }catch(error){
                 console.log("Failed to get books ", error)
