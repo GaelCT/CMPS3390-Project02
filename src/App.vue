f2a32cec460b753c18c31c856c2caf027f3bf6bb
Gael <gaelcarrillo995@gmail.com>
2025-10-11 12:01:06 -0700

    Opened a new branch and started

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import search from './componets/Search.vue';
+import Home from './Home src/Home.vue';
+
 </script>
 
 <template>
+  <Home></Home>
   <div>
     <h1>Search for a book!</h1>
     <search />
---
+++ src/Home src/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2>Hello Vishwas</h2>
+</template>
+<script>
+    export default {
+        name : 'Home'
+    }
+</script>
