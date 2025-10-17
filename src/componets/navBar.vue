8bb4d166f965c4bf3abefd3d282367726a750e58
Gael <gaelcarrillo995@gmail.com>
2025-10-17 11:47:35 -0700

    Started the nav bar

--- src/componets/navBar.vue
+++ src/componets/navBar.vue
@@ -1,6 +1,47 @@
 
 <template>
+    <div> 
+        <ul> 
+            <li v-for="nav in navItems">
+                {{ nav }}    
+
+            </li>
+        </ul>
+    </div>
+    
+    
+    <head> 
+        <body> 
+            <ul> 
+                <li><a href ="http://localhost:5173/"> Home </a> </li>
+                <li><a href =#add the search page> Search </a> </li>
+                <li><a href =#add the shopping page> Shopping </a> </li>
+            </ul>
+        </body>
+    </head>
 </template>
 
-<script setup> 
+<script> 
+    export default{
+        data(){
+            return{
+            navItems:['Home', 'Search','Cart']
+        }
+    }
+    }
+
 </script>
+<style scoped>
+li{
+    display: inline;
+    width: 100px;
+    background-color: black;
+    color: white;
+    padding: 10px;
+    margin: 5px;
+
+}
+
+li
+
+</style>
