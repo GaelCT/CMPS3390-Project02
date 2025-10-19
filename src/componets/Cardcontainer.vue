c1c1c00e6c1ed3f7bb9074423b2aa6eaa8b1a009
Gael <gaelcarrillo995@gmail.com>
2025-10-18 23:37:31 -0700

    WHY DID MY CODE ERASE

--- src/componets/Cardcontainer.vue
+++ src/componets/Cardcontainer.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="card-container">
         <h3>{{ title }}</h3>
-        <img v-if='imageSrc' :src="imageSrc" alt="Card Image" class="card-image" />
+        <img v-if='imageSrc' :src="imageSrc" alt="Card Image" class="card-image"  />
+  <!-- <button @click>-->
         
         <br>
         <br>
+
+        <!-- isnt the same type as vue, :(
             <div class = "buttonName">
-                <button> Add to Cart </button>
-                 <button> Learn More </button>    
-            </div>    
+               <button id="addToCart"> Add to Cart </button>    
+            </div>
+        -->
+
+  <button @click="modalOpen = true"> Description </button>
+  <button id="addtocart" @click="addToCart"> Add to Cart </button>
+  <div class="modal" :class="{ open: modalOpen }">
+    <div class="modal-inside">
+        <h3> Description </h3>
+            <p> {{ description }} </p>
+        <button @click="modalOpen = false"> Close </button>
+        <!----data.cardData.description{i}--->
+    </div>
   </div>
+
+</div>
 </template>
 
 
 <script setup> //YOU ABSOLUTELY NEED THIS SETUP PART TO HAVE THE BOOKS IMPORTED INTO THE CONTAINER.
-import { defineProps } from 'vue';
+import { ref, defineProps } from 'vue';
+
+const modalOpen = ref(false);
 
 defineProps({ // this is needed to pass data from a parent component to a child component. Rules for it
     title: {
@@ -26,6 +43,10 @@ defineProps({ // this is needed to pass data from a parent component to a child 
         required: false,
         default: '',
     },
+    description: {
+        type: String,
+        required: true,
+    }
 })
 
 </script>
@@ -46,21 +67,75 @@ img.card-image {
    display: flex;
    justify-content: center;
    max-width: 40%;
-   height: auto; 
+   height: 50%; 
    border-radius: 4px;
    margin: auto;
    margin-bottom: 15px;
-   
+
 }
 
 h3{
    text-align: center;
    }
 
-.buttonName{
-     display: flex;
-     justify-content: center;
-     gap: 30%;
+   .addtocart{
+background-image: linear-gradient(to right,  rgb(73, 89, 159), rgb(73, 89, 159));
+    border-radius: 31.4159265359px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .141592659);
+    color: black;
+    padding: 10px 25px;
 }
+
+button {
+    background-image: linear-gradient(to right,  rgb(165, 192, 214), rgb(165, 192, 214));
+    border-radius: 31.4159265359px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .141592659);
+    color: black;
+    padding: 10px 25px;
+}
+
+button:active {
+    opacity: 0.8;
+}
+
+
+
+
+
+.modal {
+    background-color: rgba(0, 0, 0, .141592659);
+    opacity: 0;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    transition: all 0.3 ease-in-out;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    pointer-events: none;
+}
+
+.modal.open {
+    opacity: 1;
+    z-index: 999;
+    pointer-events: auto;
+}
+
+.modal-inside{
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .141592659);
+    padding: 15px 25px;
+    text-align: center;
+    width: 300px;
+}
+
+.Popup-Modal{
+    margin: 0;
+}
+
+
 
 </style>
