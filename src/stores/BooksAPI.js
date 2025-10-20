e85410270ed433e83aaa63d93c57ed8e5bd783ea
Armando Vargas <avargas88@csub.edu>
2025-10-20 14:10:15 -0700

    testing stuff

--- src/stores/BooksAPI.js
+++ src/stores/BooksAPI.js
@@ -11,13 +11,16 @@ export const useBookStore = defineStore('bookResults', {
                 const query = encodeURIComponent(search.trim()) // saves the search to a value
                 console.log(query)
                 const response = await axios.get("https://openlibrary.org/search.json?title=" + query) 
-                this.books = response.data.docs
-                //this.books.push[{
-                    //author: response.data.docs.author,
-                    //image:
-                    //desc: response.data.docs.desc || "" dont forget to add the posilibity if empty
-                    //quantity: 0
-                //}]
+                console.log(response)
+                //this.books = response.data.docs       //old way
+                // there should be a loop
+                this.books.push[{
+                    author: response.data.docs.author_name[0],
+                    //image: "https://covers.openlibrary.org/b/id/" + response.data.docs.book.cover_i + "-L.jpg" || "https://thumbs.dreamstime.com/b/no-image-vector-symbol-missing-available-icon-gallery-moment-placeholder-277514873.jpg",
+                    desc: response.data.docs.desc[0] || "No ", //dont forget to add the posilibity if empty
+                    quantity: 0
+                }];
+                console.log(this.books)
             }catch(error){
                 console.log("Failed to get books ", error)
             }
